26ab0c3c33c8f9eeed686b6d0103405770e604f1
Nensi Pansuriya <nensipansuriya1311@gmail.com>
2025-11-10 15:42:08 +0530

    simple llm call using open router api

--- 1_simple_llm_call.py
+++ 1_simple_llm_call.py
@@ -2,12 +2,20 @@ from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
 from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+import os 
 
 load_dotenv()
 
 prompt = PromptTemplate.from_template("{question}")
 
-model = ChatOpenAI()
+openrouter_api_key = os.environ.get("OPENROUTER_API_KEY")
+
+model = ChatOpenAI(
+    api_key=openrouter_api_key, 
+    base_url="https://openrouter.ai/api/v1", 
+    model="google/gemini-2.5-flash",
+    model_kwargs={"max_tokens": 500} 
+)
 parser = StrOutputParser()
 
 chain = prompt | model | parser
